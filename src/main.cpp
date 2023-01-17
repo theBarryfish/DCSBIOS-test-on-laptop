a20459a40b1937388c536d02888684db79545b49
theBarryfish <thebarryfish@gmail.com>
2023-01-16 23:40:59 -0500

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,7 @@
 #define DCSBIOS_DEFAULT_SERIAL
 #include "DcsBios.h"  //rs485 is in there too
 
-/* Declare a Master Caution Reset button on pin 10 */
-//DcsBios::Switch2Pos masterCautionBtn("UFC_MASTER_CAUTION",32 );
-//DcsBios::LED masterCaution(0x1012, 0x0800, 32); //ufc light
-//DcsBios::LED aoaIndexerHigh(0x7408, 0x0008, 13);
-//DcsBios::LED aoaIndexerNormal(0x7408, 0x0008, 13);
-//DcsBios::LED aoaIndexerLow(0x7408, 0x0008, 13);
+
 DcsBios::LED aoaIndexerHigh(0x1012, 0x1000, 32);//green
 DcsBios::LED aoaIndexerLow(0x1012, 0x4000, 27);//red
 DcsBios::LED aoaIndexerNormal(0x1012, 0x2000, 33);//yel
